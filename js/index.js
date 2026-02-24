ca1d4e5257028c3478f18e38c6fcf362e3a276ea
Pedh27 <sousa.pedro27@escola.pr.gov.br>
2026-02-24 19:55:29 -0300

    teste de mov

--- js/index.js
+++ js/index.js
@@ -10,3 +10,23 @@ const obj = {
         },
     },
 };
+
+const cubo= document.getElementById("cubo")
+
+//Movimentando com o teclado
+
+let x = 200
+let y = 200
+const velocidade = 5
+
+document.addEventListener('Keydown', (event) => {
+    
+    if (event.key === "w") y -= velocidade;
+    if (event.key === "s") y += velocidade;
+    if (event.key === "a") x -= velocidade;
+    if (event.key === "d") x += velocidade;
+
+    cubo.style.left = x + "px"
+    cubo.style.top = y + "px"
+    
+});
