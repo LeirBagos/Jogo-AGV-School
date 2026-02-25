7fa5d142ec7aa36cfb28168568b55408bac4c543
LeirBagos <gabriel.costa.barbosa@escola.pr.gov.br>
2026-02-24 21:00:02 -0300

    sei la cara

--- js/index.js
+++ js/index.js
@@ -11,16 +11,15 @@ const obj = {
     },
 };
 
-const Cubo= document.getElementById("Cubo")
-
+const Cubo = document.getElementById("Cubo");
+s;
 //Movimentando com o teclado
 
 let x = 200;
 let y = 200;
 const velocidade = 5;
 
-document.addEventListener("Keydown", function(event) {
-    
+document.addEventListener("Keydown", function (event) {
     if (event.key === "w") y -= velocidade;
     if (event.key === "s") y += velocidade;
     if (event.key === "a") x -= velocidade;
@@ -28,5 +27,4 @@ document.addEventListener("Keydown", function(event) {
 
     Cubo.style.left = x + "px";
     Cubo.style.top = y + "px";
-
 });
