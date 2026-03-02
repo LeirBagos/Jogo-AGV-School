28f18055f086d3d98ddbf6cd924df22132b69289
Pedh27 <sousa.pedro27@escola.pr.gov.br>
2026-03-02 17:42:57 -0300

    popo

--- js/index.js
+++ js/index.js
@@ -15,7 +15,6 @@ const obj = {
 //Movimentando com o teclado
 
 const cubo = document.getElementById("cubo");
-const bloco = document.getElementById("bloco")
 
 let hspd = 0;
 let vspd = 0;
@@ -25,8 +24,6 @@ const vel = 20;
 
 document.addEventListener('keydown', (e) => {
     
-    //let newX = x;
-    //let newY = y;
 
     let up = (e.key === 'w');
     let down = (e.key === 's');
@@ -39,39 +36,7 @@ document.addEventListener('keydown', (e) => {
     x += hspd;
     y += vspd;
 
-    /*if (!checkCollision(newX, newY)){
-
-        x = newX;
-        y = newY;
-    };*/
-
     cubo.style.top = y + 'px';
     cubo.style.left = x + 'px';
-    cubo.style.top.left = (x + y /2) + "px"
 
 });
-
-/*function checkCollision(newX, newY){
-
-    const playerRect = {
-        left: newX,
-        right: newX + 40,
-        top: newY,
-        bottom: newY + 40
-    };
-
-    const blocoRect = {
-
-        left: bloco.offsetleft,
-        right: bloco.offsetleft + bloco.offsetWidth,
-        top: bloco.offsetTop,
-        bottom: bloco.offsetTop + bloco.offsetHeight
-    };
-
-    return (
-        playerRect.right > blocoRect.left &&
-        playerRect.left < blocoRect.right &&
-        playerRect.bottom > blocoRect.top &&
-        playerRect.top < blocoRect.bottom
-    );
-}*/
